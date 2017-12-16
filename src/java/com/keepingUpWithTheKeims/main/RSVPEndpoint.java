22e542fbe9322e1f528884dd581070b3e38167b5
Keim <zackkeim@186590dffa19.ant.amazon.com>
2017-12-16 13:53:24 -0800

    Add write party ability

--- src/java/com/keepingUpWithTheKeims/main/RSVPEndpoint.java
+++ src/java/com/keepingUpWithTheKeims/main/RSVPEndpoint.java
@@ -5,6 +5,17 @@
  */
 package com.keepingUpWithTheKeims.main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -25,11 +36,14 @@ public class RSVPEndpoint {
 
     @Context
     private UriInfo context;
+    private static final Logger logger =
+            Logger.getLogger(RSVPEndpoint.class.getName());    
 
     /**
      * Creates a new instance of RSVPEndpoint
      */
     public RSVPEndpoint() {
+        logger.setLevel(Level.ALL);
     }
 
     /**
@@ -62,8 +76,70 @@ public class RSVPEndpoint {
     @Consumes(MediaType.APPLICATION_JSON) 
     @Produces(MediaType.APPLICATION_JSON)
     public ResponseObject postJsonTest(RequestObject requestObject){
-        ResponseObject response = new ResponseObject();
+        ResponseObject response = new ResponseObject();                      
         
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/zackkeim/Documents/wedding-website/workspace/wedding.db");
+            connection.setAutoCommit(false);
+            logger.log(Level.INFO,"Opened database successfully");
+            
+            //Get Date               
+            java.util.Date utilDate = new java.util.Date();
+            Date creationDate = new Date(utilDate.getTime());
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            //Check if entry already in table
+            
+            String checkQuery = "select email from party where email = ? limit 1;";
+            PreparedStatement checkStatement = connection.prepareStatement(checkQuery);
+            checkStatement.setString(1, requestObject.getEmail());
+            
+            ResultSet resultSet = checkStatement.executeQuery();
+            resultSet.next();
+            if(resultSet.getString("email").isEmpty()){
+                            
+                String insertQuery = "insert into party values(?, ?, ?, ?, ?, ?);";
+
+                PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+
+                preparedStatement.setString(1, requestObject.getPartyName());
+                preparedStatement.setInt(2, requestObject.getPartyNumber());
+                preparedStatement.setString(3, requestObject.getEmail());
+                preparedStatement.setBoolean(4, requestObject.isAttending());
+                preparedStatement.setString(5, df.format(creationDate));
+                preparedStatement.setString(6, df.format(creationDate));
+
+                logger.log(Level.INFO, "Executing Insert Statement...");
+                preparedStatement.executeUpdate();
+            
+                connection.commit();
+            } else {
+                String updateQuery = "update party set party_name = ?, party_number = ?, attending = ?, last_updated_date = ? where email = ?";
+                
+                PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+
+                preparedStatement.setString(1, requestObject.getPartyName());
+                preparedStatement.setInt(2, requestObject.getPartyNumber());                
+                preparedStatement.setBoolean(3, requestObject.isAttending());
+                preparedStatement.setString(4, df.format(creationDate));                
+                preparedStatement.setString(5, requestObject.getEmail());
+
+                logger.log(Level.INFO, "Executing Update Statement...");
+                preparedStatement.executeUpdate();
+                
+                connection.commit();
+            }
+            connection.close();                 
+        } catch(ClassNotFoundException e){
+            logger.log(Level.SEVERE, "Something went wrong! ClassNotFoundException");
+            logger.log(Level.SEVERE, e.getMessage());
+            logger.log(Level.SEVERE, e.getStackTrace().toString());
+        } catch(SQLException e){
+            logger.log(Level.SEVERE, "SQLException!");
+            logger.log(Level.SEVERE, e.getMessage());
+            logger.log(Level.SEVERE, e.getStackTrace().toString());
+        }
         if (requestObject.isAttending()){
             String thankYou = "Thank You "+requestObject.getPartyName()+"! We look forward to seeing you on the big day!";            
             response.setResponse(thankYou);
